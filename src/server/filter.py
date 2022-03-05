d67c562440084ba637ea4e73c182d91c7b22ddc0
ddaaggeett <david.g.daggett@gmail.com>
2022-03-04 21:21:33 -0500

    filter every color except white

--- src/server/filter.py
+++ src/server/filter.py
@@ -11,11 +11,12 @@ def filter(image):
     height, width, color = image.shape
     blank_image = create_blank(width,height)
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    lower_green = numpy.array([50,100,50])
-    upper_green = numpy.array([70,255,255])
-    mask = cv2.inRange(hsv, lower_green, upper_green)
-    background_mask = cv2.bitwise_not(mask)
+    # Every color except white
+    low = numpy.array([0,0,0])
+    high = numpy.array([359, 255, 255])
+    maskInk = cv2.inRange(hsv, low, high)
+    inkResult = cv2.bitwise_and(image, image, mask=maskInk)
+    background_mask = cv2.bitwise_not(maskInk)
     background = cv2.bitwise_or(blank_image, blank_image, mask=background_mask)
-    mask_green= cv2.bitwise_or(image, image, mask=mask)
-    filtered = cv2.bitwise_or(mask_green, background)
+    filtered = cv2.bitwise_or(inkResult, background)
     return(filtered)
