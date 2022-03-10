3036877052102a6a0b5d65c1a1f40a7a29629132
ddaaggeett <david.g.daggett@gmail.com>
2022-03-10 08:34:45 -0500

    server finds its own ip address upon startup

--- src/server/imageData_init.js
+++ src/server/imageData_init.js
@@ -1,7 +1,17 @@
 const fs = require('fs')
 const path = require('path')
-const { imageData } = require('../../config')
+const config = require('../../config')
+const { exec } = require('child_process')
 
-fs.mkdir(path.join(imageData), {recursive:true}, error => {
+fs.mkdir(path.join(config.imageData), {recursive:true}, error => {
     if(error) console.error(error)
+    exec('hostname -I', (error, stdout, stderr) => {
+        if (!error) {
+            const cut = stdout.indexOf(' \n')
+            const serverIP = stdout.slice(0,cut)
+            config.serverIP = serverIP
+            fs.writeFile(config.configFile, JSON.stringify(config, null, 4), (error) => {
+            })
+        }
+    })
 })
