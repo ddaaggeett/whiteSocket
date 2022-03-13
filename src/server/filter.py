0e1a6ef7364459b3c856013949bbdb40c62682fa
ddaaggeett <david.g.daggett@gmail.com>
2022-03-13 11:22:31 -0400

    init eraser areas mask

--- src/server/filter.py
+++ src/server/filter.py
@@ -18,5 +18,14 @@ def inkOnly(image):
     inkResult = cv2.bitwise_and(image, image, mask=maskInk)
     background_mask = cv2.bitwise_not(maskInk)
     background = cv2.bitwise_or(blank_image, blank_image, mask=background_mask)
-    filtered = cv2.bitwise_or(inkResult, background)
-    return(filtered)
+    inkOnly = cv2.bitwise_or(inkResult, background)
+    return(inkOnly)
+
+def eraseAreas(image):
+    # return mask
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    eraseAreas = cv2.Canny(gray, 30, 200)
+    contours, hierarchy = cv2.findContours(eraseAreas, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.fillPoly(eraseAreas,contours, color=(255,255,255))
+    cv2.imshow('Contours', eraseAreas)
+    return(eraseAreas)
