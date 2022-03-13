8dd2a627658584390a58378718b5e8f07a558314
ddaaggeett <david.g.daggett@gmail.com>
2022-03-13 19:39:35 -0400

    mask region of interest

--- src/server/mask.py
+++ src/server/mask.py
@@ -29,3 +29,27 @@ def eraseAreas(image):
     cv2.fillPoly(eraseAreas,contours, color=(255,255,255))
     cv2.imshow('Contours', eraseAreas)
     return(eraseAreas)
+
+def roi(image):
+    mask = numpy.zeros(image.shape[:2], dtype="uint8")
+    inputCorners = numpy.zeros((4,2))
+    arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+    arucoParams = cv2.aruco.DetectorParameters_create()
+    corners, ids, rejected = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+    if len(ids) == 4:
+        ids = ids.flatten()
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4, 2))
+            (tl, tr, br, bl) = corners
+            tl = [int(tl[0]), int(tl[1])]
+            tr = [int(tr[0]), int(tr[1])]
+            br = [int(br[0]), int(br[1])]
+            bl = [int(bl[0]), int(bl[1])]
+            # outer-most corner of each arcuro
+            if markerID == 0: inputCorners[0] = tl
+            if markerID == 1: inputCorners[1] = tr
+            if markerID == 2: inputCorners[2] = br
+            if markerID == 3: inputCorners[3] = bl
+    contours = numpy.array([inputCorners[0],inputCorners[1],inputCorners[2],inputCorners[3]])
+    cv2.fillPoly(mask, pts = numpy.int32([contours]), color =(255,255,255))
+    return(mask)
