4ab028ad2b9afbf1817456d173d17420a60251a1
ddaaggeett <david.g.daggett@gmail.com>
2022-03-31 18:11:59 -0400

    dedicated whitesocket server directory

--- src/server/whitesocket/index.js
+++ src/server/whitesocket/index.js
@@ -1,13 +1,13 @@
 const { exec } = require('child_process')
 const path = require('path')
-const config = require('../../config')
+const config = require('../../../config')
 
 const whitesocket = diff => {
     const input = path.join(config.imageData, diff.uri)
     const output = path.join(config.imageData, diff.result)
     const mode = diff.mode
     return new Promise(function(resolve, reject) {
-        exec(`python ./src/server/index.py ${input} ${output} ${mode}`, (error, stdout, stderr) => {
+        exec(`python ./src/server/whitesocket/index.py ${input} ${output} ${mode}`, (error, stdout, stderr) => {
             if(!error) resolve()
         })
     })
