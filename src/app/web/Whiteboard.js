e30f1370b1817e9d7969349b0d1dd2186628ef55
ddaaggeett <david.g.daggett@gmail.com>
2022-04-07 15:28:49 -0400

    white background image separate from overlay output image

--- src/app/web/Whiteboard.js
+++ src/app/web/Whiteboard.js
@@ -1,13 +1,14 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { View, Image, StyleSheet } from 'react-native'
+import { ImageBackground, View, Image, StyleSheet } from 'react-native'
 const config = require('../../../config')
 
 export default (props) => {
 
     const { diff, prepping } = useSelector(state => state.app)
-    const whiteboardRef = useRef()
     const [fullscreen, setFullscreen] = useState(false)
+    const [backgroundHeight, setBackgroundHeight] = useState(window.innerHeight)
+    const [backgroundWidth, setBackgroundWidth] = useState(window.innerWidth)
     const [height, setHeight] = useState(window.innerHeight)
     const [width, setWidth] = useState(window.innerWidth)
     const imageBaseURI = `http://${config.serverIP}:${config.expressPort}/`
@@ -21,35 +22,21 @@ export default (props) => {
         setImageURI(`${imageBaseURI}${diff.result_uri}`)
     }, [diff])
 
-    const handleFullscreen = () => {
-        if(!fullscreen) {
-            whiteboardRef.current.requestFullscreen()
-            .then(() => setFullscreen(true))
-            .catch(error => console.error(error))
-        }
-        else {
-            document.exitFullscreen()
-            setFullscreen(false)
-        }
-    }
-
     useEffect(() => {
-        setHeight(window.innerHeight)
-        setWidth(window.innerWidth)
-    }, [window.innerHeight])
-
-    useEffect(() => {
-        setHeight(window.innerHeight)
-        setWidth(window.innerWidth)
-    }, [])
+        setBackgroundHeight(window.innerHeight)
+        setBackgroundWidth(window.innerWidth)
+    }, [window.innerHeight, window.innerWidth])
 
     return (
-        <Image
-            source={{ uri: imageURI }}
-            style={{width,height}}
-            ref={whiteboardRef}
-            onClick={handleFullscreen}
-            />
+        <ImageBackground
+            source={{uri: `${imageBaseURI}${config.defaultImage}`}}
+            style={{height: backgroundHeight, width: backgroundWidth}}
+            >
+            <Image
+                source={{ uri: imageURI }}
+                style={{width, height}}
+                />
+        </ImageBackground>
     )
 }
 
